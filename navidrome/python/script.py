cbe12c1e6c805fe6ffa6d2d4d783293ab7c741a1
brianlobo-procloud <brian.lobo@procloudengineers.com>
2025-06-23 18:32:11 -0300

    Agregado nuevo script para cambiar la metadata de las canciones de navidrome

---
+++ navidrome/python/script.py
@@ -0,0 +1,28 @@
+import sys
+from pathlib import Path
+
+import music_tag
+
+
+MUSIC_EXTENSIONS = ['mp3', 'flac', 'ogg', 'wav']
+
+workindir = sys.argv[1]
+
+print(f"Running script for path -> {workindir}")
+
+paths = [
+    str(p)
+    for p in Path(workindir).rglob('*')
+    if str(p).split('.')[-1].lower() in MUSIC_EXTENSIONS
+]
+
+for p in paths:
+
+    music = music_tag.load_file(p)
+
+    music['title'] = p.split('/')[-1].split('.')[0]
+    music['album'] = p.split('/')[-2]
+    music['artist'] = p.split('/')[-3]
+    music['genre'] = p.split('/')[-4]
+
+    music.save()
